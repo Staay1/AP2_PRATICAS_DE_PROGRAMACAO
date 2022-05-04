6831907d8c31b9ca810c9fca5a0ce9f634f53b26
Daniel Succar <89500097+Staay1@users.noreply.github.com>
2022-05-03 22:35:27 -0300

    Add files via upload

---
+++ app/forms.py
@@ -0,0 +1,8 @@
+from django.forms import ModelForm
+from app.models import Motos
+
+# Create the form class.
+class MotosForm(ModelForm):
+    class Meta:
+        model = Motos
+        fields = ['modelo', 'marca', 'ano']
